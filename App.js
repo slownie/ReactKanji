d7e4e95a3748bfea5d5dcf93c729654014e6f52b
Samuel <slownie16@gmail.com>
2020-03-31 13:58:41 -0400

    app base complete

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+
+{ /*Navigators */ }
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+{ /*Navigation Screens */ }
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+{ /*Stack Screens */ }
+function HomeStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name = "React Kanji"
+        component = { HomeScreen }
+        options = {{
+          headerStyle: {
+            backgroundColor: '#673AB7',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+
+{ /*Main Function  */ }
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions = {{
+          activeTintColor: '#673AB7',
+        }}
+      >
+        <Tab.Screen name = 'Home' component = { HomeStackScreen } />
+        <Tab.Screen name = 'Dictionary' component = { HomeStackScreen } />
+        <Tab.Screen name = 'Quiz' component = { HomeStackScreen } />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
